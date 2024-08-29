27159bbfd13c470d1fe6a6378d05c775e812ac95
Sander-Steinbusch <sander.steinbusch@jidoka.be>
2024-08-29 14:25:04 +0200

    change port to 8000

--- document_analyzer/configuration/arguments.py
+++ document_analyzer/configuration/arguments.py
@@ -4,7 +4,7 @@ def get_arguments():
     parser = ArgumentParser()
     parser.add_argument("-m", "--mock-data", help="return test data.", action="store_true")
     parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
-    parser.add_argument("-p", "--port", help="Specify the port to listen to. Default is 8080.", 
-                        default=8080, type=int)
+    parser.add_argument("-p", "--port", help="Specify the port to listen to. Default is 8000.",
+                        default=8000, type=int)
     return parser.parse_args()
     
