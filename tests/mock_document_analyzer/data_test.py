4bd560e2205131c2b8ddc8f63c2848540676166e
Sander-Steinbusch <sander.steinbusch@jidoka.be>
2024-08-30 08:44:27 +0200

    More test clean up

--- tests/mock_document_analyzer/data_test.py
+++
@@ -1,9 +0,0 @@
-from document_analyzer.models.offerte_response import Offerte
-from mock_document_analyzer.data import get_test_data
-
-def test_data_conforms_to_model():
-    data: dict = get_test_data()
-
-    model = Offerte.model_validate(data)
-
-    assert model is not None
